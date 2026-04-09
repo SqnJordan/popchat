609de8cccce1c3afdbce92d5ef0ed1ae0bdc888b
SqnJordan <sangiorda@gmail.com>
2026-04-09 15:20:07 -0300

    Update server.js

--- server.js
+++ server.js
@@ -105,7 +105,6 @@ function conectarUsuarios(u, v){
   u.pareja = v;
   v.pareja = u;
 
-  // mensaje
   u.ws.send(JSON.stringify({
     type:"message",
     data:{texto:"¡Conectado con alguien!", usuario:"Sistema"}
@@ -116,7 +115,6 @@ function conectarUsuarios(u, v){
     data:{texto:"¡Conectado con alguien!", usuario:"Sistema"}
   }));
 
-  // info pareja
   u.ws.send(JSON.stringify({
     type:"infoPareja",
     data: v
@@ -126,57 +124,6 @@ function conectarUsuarios(u, v){
     type:"infoPareja",
     data: u
   }));
-}
-
-
-  // Mensaje de conexión
-  u.ws.send(JSON.stringify({
-    type:"message",
-    data:{texto:"¡Conectado con alguien!", usuario:"Sistema"}
-  }));
-
-  v.ws.send(JSON.stringify({
-    type:"message",
-    data:{texto:"¡Conectado con alguien!", usuario:"Sistema"}
-  }));
-
-  // 👇 ENVIAR INFO DE LA PAREJA
-  u.ws.send(JSON.stringify({
-    type:"infoPareja",
-    data:{
-      nombre: v.nombre,
-      edad: v.edad,
-      genero: v.genero,
-      pelicula: v.pelicula,
-      deporte: v.deporte,
-      clima: v.clima,
-      color: v.color,
-      comida: v.comida,
-      club: v.club,
-      sueno: v.sueno
-    }
-  }));
-
-  v.ws.send(JSON.stringify({
-    type:"infoPareja",
-    data:{
-      nombre: u.nombre,
-      edad: u.edad,
-      genero: u.genero,
-      pelicula: u.pelicula,
-      deporte: u.deporte,
-      clima: u.clima,
-      color: u.color,
-      comida: u.comida,
-      club: u.club,
-      sueno: u.sueno
-    }
-  }));
-
-  break;
-}
-    }
-  }
 }
 
 const PORT = process.env.PORT || 3000;
